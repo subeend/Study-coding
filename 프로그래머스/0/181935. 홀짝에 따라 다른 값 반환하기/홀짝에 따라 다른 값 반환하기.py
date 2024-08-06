c5c92de0d873b656de24c0d8adae3d3a0c9b7b0e
subeeng <soorud05@kyonggi.ac.kr>
2024-08-06 23:51:15 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    sum=0
+    if n%2==0:
+        for i in range(1,n+1):
+            if i%2==0:
+                sum+=i*i
+            
+    elif n%2==1:
+        for i in range(1,n+1):
+            if i%2==1:
+                sum+=i
+            
+    return sum
+            
